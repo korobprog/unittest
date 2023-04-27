894f4a1ecfe9a0b45540cf39907b1d3f46e98f3d
korobprog <korobprog@gmail.com>
2023-04-27 15:07:31 +1000

    config error

---
+++ src/js/streakoflife.js
@@ -0,0 +1,17 @@
+
+const heroes = [
+   { name: 'мечник', health: 10 },
+   { name: 'маг', health: 100 },
+   { name: 'лучник', health: 80 },
+ ];
+ 
+ heroes.sort((a, b) => {
+   if (a.health > b.health) {
+     return 1;
+   }
+   if (a.health < b.health) {
+     return -1;
+   }
+   // a должно быть равным b
+   return 0;
+ });
